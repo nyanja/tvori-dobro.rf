3d7974d554943911b0745a28dcd1be6907f43d3d
nyan <1eaegue@gmail.com>
2018-05-23 17:31:00 +0300

    add more info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
       .render(md)
       .html_safe
   end
+
+  def site_phone
+    "7 (978) 142-58-12"
+  end
+
+  def site_mail
+    "fond-tvori-dobro@mail.ru"
+  end
 end
